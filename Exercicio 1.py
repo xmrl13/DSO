ade2ef119167df91a4ec409331bf9434921b7fb7
Marcelo de Oliveira <109050359+xmrl13@users.noreply.github.com>
2022-08-27 16:54:13 -0300

    Update Exercicio 1.py

--- Exercicio 1.py
+++ Exercicio 1.py
@@ -3,35 +3,40 @@
    INE - Departamento de Informatica e Estatistica - http://inf.ufsc.br
 """
 
-
 class Ordenacao():
 
-    def __init__(self, array_para_ordenar):
-        """Recebe o array com o conteudo a ser ordenado"""
-        self.array = array_para_ordenar
+    def __init__(self, array_para_ordenar:[]):
+
+        self.array_para_ordenar = array_para_ordenar
 
     def ordena(self):
 
-        self.array
-		 # Faz a mágica de ordenar
-        """Realiza a ordenacao do conteudo do array recebido no construtor................................."""
-        self.array  # Faz a mágica de orden
-        """Realiza a ordenacao do conteudo do array recebido no construtor"""
-        self.array  # Faz a mágica de ordenar
+        array_do_ordena = self.array_para_ordenar
+        array_ordenado = []
 
-        return ...
+        for numero in array_do_ordena:
+            for chave, valor in enumerate(array_ordenado):
+                if numero < valor:
+                    array_ordenado.insert(chave, numero)
+                    break
+            else:
+                array_ordenado.append(numero)
+        array_para_ordenar = array_ordenado
+        return array_para_ordenar
 
     def toString(self):
-        """Converte o conteudo do array em String formatado
-           Exemplo:
-           Para o conteudo do array: [1,2,3,4,5]
-           Retorna: "1,2,3,4,5"
-           @return String com o conteudo do array formatado
-     """
 
-        self.array  # Faz a mágica de ordenar
-        return ...
+        nova_string = ''
+        string_ordenado = self.ordena()
+        string_ordenado = str(string_ordenado).strip('[]')
+        string_ordenado = str(string_ordenado).strip(' ')
+        for i in range(len(string_ordenado)):
+            if string_ordenado[i] != ' ':
+                nova_string = nova_string+string_ordenado[i]
+        print(nova_string)
+        print(type(nova_string))
+        return nova_string
 
-
-sequencia = Ordenacao('1,2,3,4,5,6,7')
-print(type(sequencia.array))
+ordenar = Ordenacao([9,4,2,8,4,13,54,87,1])
+ordenar.ordena()
+ordenar.toString()

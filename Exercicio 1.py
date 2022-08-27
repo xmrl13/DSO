78a15be67e3d7fec9ab0e7bf743eff83f953ad01
Marcelo de Oliveira <109050359+xmrl13@users.noreply.github.com>
2022-08-26 23:29:19 -0300

    Update Exercicio 1.py

---
+++ Exercicio 1.py
@@ -0,0 +1,28 @@
+"""Universidade Federal de Santa Catarina.
+   CTC - Centro Tecnologico - http://ctc.ufsc.br
+   INE - Departamento de Informatica e Estatistica - http://inf.ufsc.br
+"""
+
+
+class Ordenacao():
+
+    def __init__(self, array_para_ordenar: []):
+        """Recebe o array com o conteudo a ser ordenado"""
+        self.array = array_para_ordenar
+
+    def ordena(self):
+        """Realiza a ordenacao do conteudo do array recebido no construtor"""
+        self.array  # Faz a mágica de ordenar
+
+        return ...
+
+    def toString(self):
+        """Converte o conteudo do array em String formatado
+           Exemplo:
+           Para o conteudo do array: [1,2,3,4,5]
+           Retorna: "1,2,3,4,5"
+           @return String com o conteudo do array formatado
+     """
+
+        self.array  # Faz a mágica de ordenar
+        return ...

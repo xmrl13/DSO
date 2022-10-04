9fdcafb23273cbc6d5e993984c12aaca6d95f3b8
Marcelo de Oliveira <109050359+xmrl13@users.noreply.github.com>
2022-10-03 21:58:19 -0300

    Upload MVC

---
+++ MVC/controle/controlador_principal.py
@@ -0,0 +1,25 @@
+import sys
+
+from limite.tela_principal import TelaPrincipal
+from controle.controlador_cliente import ControladorCliente
+
+class ControladorPrincipal:
+
+    def __init__(self):
+        self.__tela_principal = TelaPrincipal()
+        self.__controlador_cliente = ControladorCliente(self)
+
+    def inicia_clientes(self):
+        self.__controlador_cliente.mostra_tela_opcoes()
+
+    def inicia_notas_fiscais(self):
+        print('NOTAS FISCAIS')
+
+    def finaliza(self):
+        sys.exit()
+
+    def inicia(self):
+        opcoes = {1: self.inicia_clientes, 2: self.inicia_notas_fiscais, 0: self.finaliza}
+        while True:
+            opcao = self.__tela_principal.mostra_tela_inicial()
+            opcoes[opcao]()
